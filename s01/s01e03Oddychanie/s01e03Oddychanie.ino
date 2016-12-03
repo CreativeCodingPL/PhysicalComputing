7c4785bc56931108902ccaf3f419cf268db506b4
wieslaw bartkowski <wieslaw.bartkowski@sof.edu.pl>
2016-12-03 15:03:12 +0100

    kody do zajęć 01

--- s01/s01e03Oddychanie/s01e03Oddychanie.ino
+++ s01/s01e03Oddychanie/s01e03Oddychanie.ino
@@ -1,11 +1,5 @@
-#include <Servo.h>
-
-Servo serwus;
-
 void setup() {
-
-  serwus.attach(9);
-  
+  pinMode(9, OUTPUT); 
 }
 
 float k = 0;
@@ -13,7 +7,7 @@ float k = 0;
 void loop() {
   int wartosc = 1000*sin(k);
   int jasnosc = map(wartosc, -1000, 1000, 20, 160);
-  serwus.write(jasnosc);
+  analogWrite(9, jasnosc);
   k = k + 0.003;
   delay(1);
 }
