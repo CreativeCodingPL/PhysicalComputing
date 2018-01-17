063849f624672e54f85b69705d904a59162eac33
Krzysztof Goliński <krzysztof@golinski.org>
2018-01-17 19:08:37 +0100

    s07 🕹

---
+++ s07/joystick/joystick.ino
@@ -0,0 +1,19 @@
+void setup() {
+  pinMode(2, INPUT);
+  Serial.begin(9600);
+  digitalWrite(2, HIGH);
+}
+
+void loop() {
+  Serial.print(analogRead(A0));
+  Serial.print(';');x
+  Serial.print(analogRead(A1));
+  Serial.print(';');
+  if(digitalRead(2)==HIGH){
+    Serial.println(0);
+  }
+  else {
+    Serial.println(1);
+  }
+  delay(100);
+}
