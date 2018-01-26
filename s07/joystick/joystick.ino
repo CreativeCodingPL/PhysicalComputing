1cd761125a0d284c7179bb07e888a3ca446d1753
Krzysztof Goliński <krzysztof@golinski.org>
2018-01-26 09:36:19 +0100

    wróć!

--- s07/joystick/joystick.ino
+++ s07/joystick/joystick.ino
@@ -1,7 +1,3 @@
-int tryb = 0;
-//0 - normalne liczby
-//1 - przyciete liczby
-
 void setup() {
   pinMode(2, INPUT);
   Serial.begin(9600);
@@ -9,27 +5,16 @@ void setup() {
 }
 
 void loop() {
-  if (digitalRead(2) == LOW) {
-    if(tryb==0) { 
-      tryb = 1;
-    }
-    else if(tryb==1) {
-      tryb=0;
-    }
-  }
-  if (tryb == 0) {
-    Serial.print(analogRead(A0));
-    Serial.print(';');
-    Serial.print(analogRead(A1));
-    Serial.print(';'); 
+  Serial.print(analogRead(A0));
+  Serial.print(';');x
+  Serial.print(analogRead(A1));
+  Serial.print(';');
+  if(digitalRead(2)==HIGH){
     Serial.println(0);
   }
-  else if (tryb == 1) {
-    Serial.print(map(analogRead(A0),0, 1023, 255, 768));
-    Serial.print(';');
-    Serial.print(map(analogRead(A1),0, 1023, 255, 768));
-    Serial.print(';'); 
+  else {
     Serial.println(1);
   }
   delay(100);
 }
+
