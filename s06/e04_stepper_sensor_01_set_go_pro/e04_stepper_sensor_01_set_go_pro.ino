6ecb21a738b5aab55bb973a793dc00107efa6143
Wiesław Bartkowski <wbartkowski@users.noreply.github.com>
2021-02-03 21:41:07 +0100

    Sezon 06 - stepper przyciski i pozycjonowanie czujnikiem

--- s06/e04_stepper_sensor_01_set_go_pro/e04_stepper_sensor_01_set_go_pro.ino
+++ s06/e04_stepper_sensor_01_set_go_pro/e04_stepper_sensor_01_set_go_pro.ino
@@ -1,7 +1,7 @@
 #include <AccelStepper.h>
 
 //zmineiłem kolejność kabli, żeby ładniej wyglądało 
-//na rysunku z połaczoniemi :)
+//na rysunku z połączeniami :)
 #define in1 11
 #define in2 10
 #define in3 9
@@ -21,8 +21,8 @@ int krok = 1;
 
 void loop() {
   
-  //jak znajdzie poczatek to zakrywa sensor więć miej niż 150
-  //zaczynamy od tego bo może się zdażyć, że przypadkiem na starcie
+  //jak znajdzie początek to zakrywa sensor więć miej niż 150
+  //zaczynamy od tego bo może się zdarzyć, że przypadkiem na starcie
   //jest już na początku
   if(analogRead(A0) < 150) {
       nie_znaleziony = false; 
@@ -31,17 +31,16 @@ void loop() {
   //jak nie znalazl początku to przesuwa o 250 kroków
   if (nie_znaleziony) { 
     stepper.move(250); 
-    //move(50) - to samo co - moveTo(stepper.currentPosition()+50);
+    //move(250) - to samo co - moveTo(stepper.currentPosition()+250);
   }
 
   //potem wszyko inne, czyli zasadnicze ruchy
   if(!nie_znaleziony) {
     if(stepper.distanceToGo() == 0 && krok < 5) {
-      if(krok == 1) stepper.move(64*64); //pelen obort przeciwnie do zegara
-      if(krok == 2) stepper.moveTo(stepper.currentPosition()-64*64*0.5); //pol obortou z kierumkiem zegara
-      if(krok == 3) stepper.moveTo(stepper.currentPosition()+64*64*(3.0/4.0)); //3/4 obortu przeciwnie do zegara
-      if(krok == 4) stepper.moveTo(stepper.currentPosition()+64*64*0.5); //pol obortou przeciwnie do zegara
-      krok++;
+      if(krok == 1) stepper.move(64*64); //pelen obort zgodnie z ruchem zegara
+      if(krok == 2) stepper.move(-64*64*0.5); //pol obortou przeciwnie do ruchemu zegara
+      if(krok == 3) stepper.move(64*64*3.0/4.0); // 3/4 obortu zgodnie z ruchem zegara
+      if(krok == 4) stepper.move(64*64*0.5); //pol obortou zgodnie z ruchem zegara
       delay(1000); //na chwile staje na mejsu
     }
   }
